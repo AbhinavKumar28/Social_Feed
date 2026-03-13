3f1f9c6cb133ebdba3cf20ebf5bc38cbd5250033
Abhinav Kumar <emailofabhinav@gmail.com>
2026-03-13 22:33:05 +0530

    feat: trending card done using data (closes #1)

---
+++ Social_Feed_Frontend/src/components/NewsCard.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from "react";
+import type { News } from "../types/components";
+import "../App.css";
+function NewsCard({ news }: { news: News }): JSX.Element {
+  return (
+    <>
+      <div className="news-card">
+        <div className="width-70">
+          <div>
+            {news.source} · {news.time}
+          </div>
+          <div>{news.title}</div>
+          <div className="posts-news">
+            {news.author !== "" ? <div>By , {news.author}</div> : <div></div>}
+            <div className="margin-left">{news.posts} posts</div>
+          </div>
+        </div>
+        <div>
+          <img className="news-image" src={news.image} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+export default NewsCard;
--- Social_Feed_Frontend/src/components/TrendingCard.tsx
+++ Social_Feed_Frontend/src/components/TrendingCard.tsx
@@ -1,81 +1,31 @@
 // import { useState } from "react";
 import type { JSX } from "react";
 import "../App.css";
-import mastodonLogo from "../assets/mastodon.png";
-import aiLogo from "../assets/ai.png";
-import type { Clicked, Post } from "../types/components";
+import type { Clicked, Hashtag, News, People, Post } from "../types/components";
 import PostCard from "./PostCard";
 import postsOf from "../data/trending-posts.json";
 const posts: Post[] = postsOf;
+import hashtagsOf from "../data/trending-hashtags.json";
+import HashtagCard from "./HashtagCard";
+const hashtags: Hashtag[] = hashtagsOf;
+import peopleOf from "../data/trending-people.json";
+import PeopleCard from "./PeopleCard";
+const persons: People[] = peopleOf;
+import newsOf from "../data/trending-news.json";
+import NewsCard from "./NewsCard";
+const newspaper: News[] = newsOf;
+
 function TrendingCard({ clicked }: Clicked): JSX.Element {
   //   const [count, setCount] = useState(0);
   return (
     <>
-      {clicked === "posts" &&
-        posts.map((post: Post, index: number) => <PostCard key={index} post={post} />)}
-      {clicked === "hashtags" && (
-        <>
-          <div className="pad-bor">
-            <div className="bold">#SeabirdsABookOrPlay</div>
-            <div>83 people in the past 2 days</div>
-          </div>
-          <div className="pad-bor">
-            <div className="bold">#fruitasongorpoem</div>
-            <div>134 people in the past 2 days</div>
-          </div>
-          <div className="pad-bor">
-            <div className="bold">#TuneTuesday</div>
-            <div>97 people in the past 2 days</div>
-          </div>
-        </>
-      )}
-      {clicked === "people" && (
-        <>
-          <div className="grid-view">
-            <div className="column-one">Popular on mastodon.social</div>
+      {clicked === "posts" && posts.map((post: Post) => <PostCard key={post.id} post={post} />)}
+      {clicked === "hashtags" &&
+        hashtags.map((hashtag: Hashtag) => <HashtagCard key={hashtag.id} hashtag={hashtag} />)}
+      {clicked === "people" &&
+        persons.map((people: People) => <PeopleCard key={people.id} people={people} />)}
 
-            <div className="column-two">
-              <img src={mastodonLogo} className="people-image" alt="" />
-            </div>
-            <div className="column-one">
-              <div>Information Is Beautiful</div>
-              <div>@infobeautiful@vis.social</div>
-            </div>
-            <button className="explore-suggestions-card__dismiss-button icon-button column-three">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-                className="icon icon-close"
-              >
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"></path>
-              </svg>
-            </button>
-
-            <button className="column-four">Follow</button>
-          </div>
-        </>
-      )}
-      {clicked === "news" && (
-        <>
-          <div className="news-card">
-            <div>
-              <div>USA TODAY · 2d</div>
-              <div>
-                Deepak Chopra acknowledges &apos;poor judgment&apos; over emails with Epstein
-              </div>
-              <div className="posts-news">
-                <div>By , USA TODAY</div>
-                <div className="margin-left">160 posts</div>
-              </div>
-            </div>
-            <div>
-              <img className="news-image" src={aiLogo} alt="" />
-            </div>
-          </div>
-        </>
-      )}
+      {clicked === "news" && newspaper.map((news: News) => <NewsCard key={news.id} news={news} />)}
     </>
   );
 }
